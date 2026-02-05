f2e9878ec6a4a21f2c16e503643d4c0406271692
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-02-05 09:23:59 +0800

    Update 永乐.js

--- XB/永乐.js
+++ XB/永乐.js
@@ -83,7 +83,7 @@ async function detail(id) {
             'vod_name': (html.match(/<h1>(.*?)<\/h1>/) || ["", ""])[1],
             'vod_pic': (html.match(/data-original="(.*?)"/) || ["", ""])[1],
             'vod_content': '【琉🔹芸❤广告勿信👉剧情】 '+(html.match(/introduction-content">.*?<p>(.*?)<\/p>/s) || ["", ""])[1].replace(/<.*?>/g, ""),
-            'vod_play_from': playFrom('💕琉芸👉$$$'),
+            'vod_play_from': playFrom,
             'vod_play_url': playUrl
         }]
     });
