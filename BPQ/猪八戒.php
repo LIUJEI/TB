8fc48704652958857822467685ddbcc360becfb3
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-07 07:59:27 +0800

    Initial commit

--- BPQ/猪八戒.php
+++ BPQ/猪八戒.php
@@ -8,4 +8,4 @@
         "搜索url": "https://zbj5.lol/cn/home/web/index.php/vod/search/page/{pg}/wd/{wd}.html",
         "分类": "韩国美眉$20#骑兵有码$21#日韩主播$22#国产偷拍$23#欧美激情$24#步兵无码$25#金发幼齿$26#三级伦理$27#岛国中文$28#岛国无码$29#",
         "分类url": "https://zbj5.lol/cn/home/web/index.php/vod/type/id/{cateId}/page/{catePg}.html"
-      }
+}
